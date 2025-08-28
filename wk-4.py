882054c2bdd6a2bb114796a25516385b631a8386
ChristianSEDev <christianandani52@gmail.com>
2025-08-29 01:27:31 +0200

    pythonwk-4

--- wk-4.py
+++ wk-4.py
@@ -1,1 +1,29 @@
+# 1. Opening the input file in read mode
+with open("input.txt", "r") as infile:
+    input_content = infile.read()
 
+# Modifying the content
+modified_content = input_content.upper()
+
+# Open the output file in write mode
+with open("output.txt", "w") as outfile:
+    outfile.write(modified_content)
+
+# 2. Asking the user to enter a filename
+filename = input("Enter the filename: ")
+
+try:
+    # Prevent overwriting output.txt if user enters that filename
+    if filename == "output.txt":
+        print("Warning: You are trying to read the output file.")
+    with open(filename, "r") as file:
+        file_content = file.read()
+        print(file_content)
+
+# Handle the case where the file doesn't exist
+except FileNotFoundError:
+    print("Error: File not found.")
+
+# Handle other input/output errors
+except IOError:
+     print("Error: Cannot read the file.")
